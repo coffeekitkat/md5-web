de57cce4fdc71a17a1dbc16911dbf1de170e168d
coffeekitkat <coffeekitkat39@gmail.com>
2021-09-21 03:16:45 +0800

    chore: configure tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mode: 'jit',
-    purge: ['./src/**/*.{html,pug,js,ts}'],
+    purge: ['./src/**/*.{html,pug,js,ts,vue}'],
     separator: '_', // to make usable with 'pugjs'
     darkMode: false, // or 'media' or 'class'
     theme: {
